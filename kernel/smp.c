2444a2a4cd7332228d153488095a97a9b168eacb
Suresh Siddha <suresh.b.siddha@intel.com>
2012-04-20 17:08:50 -0700

    smp, idle: Allocate idle thread for each possible cpu during boot
    percpu areas are already allocated during boot for each possible cpu.
    percpu idle threads can be considered as an extension of the percpu areas,
    and allocate them for each possible cpu during boot.
    
    This will eliminate the need for workqueue based idle thread allocation.
    In future we can move the idle thread area into the percpu area too.
    
    [ tglx: Moved the loop into smpboot.c and added an error check when
      the init code failed to allocate an idle thread for a cpu which
      should be onlined ]
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: venki@google.com
    Link: http://lkml.kernel.org/r/1334966930.28674.245.camel@sbsiddha-desk.sc.intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Change-Id: I36828165fc08b7c0a8a0fe6a2aa24d358e623dd2
    
    Conflicts:
    	arch/arm/Kconfig
    	kernel/smp.c

--- kernel/smp.c
+++ kernel/smp.c
@@ -11,6 +11,8 @@
 #include <linux/reboot.h>
 #endif
 
+#include "smpboot.h"
+
 #ifdef CONFIG_USE_GENERIC_SMP_HELPERS
 static struct {
 	struct list_head	queue;
@@ -514,7 +516,9 @@ void __init smp_init(void)
 {
 	unsigned int cpu;
 
-	
+	idle_threads_init();
+
+	/* FIXME: This should be done in userspace --RR */
 	for_each_present_cpu(cpu) {
 		if (num_online_cpus() >= setup_max_cpus)
 			break;
