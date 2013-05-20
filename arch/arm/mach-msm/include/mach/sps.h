457e92d3b0933509256039225a0cd57fbd8bfa5d
Yan He <yanhe@codeaurora.org>
2013-05-20 13:17:29 -0700

    msm: sps: enable BAM DMA client to control BAM DMA clock
    For power saving reason, SPS driver will turn off the clock of
    BAM DMA after the initial configuration. A new API is provided
    to BAM DMA clients to vote for or relinquish BAM DMA clock when
    they start/stop using BAM DMA.
    
    Change-Id: I03f8a6cfa19c1722ca4e92e0785ca309fdd10ba2
    Signed-off-by: Yan He <yanhe@codeaurora.org>

--- arch/arm/mach-msm/include/mach/sps.h
+++ arch/arm/mach-msm/include/mach/sps.h
@@ -496,6 +496,15 @@ int sps_get_unused_desc_num(struct sps_pipe *h, u32 *desc_num);
 
 int sps_get_bam_debug_info(u32 dev, u32 option);
 
+/**
+ * Vote for or relinquish BAM DMA clock
+ *
+ * @clk_on - to turn on or turn off the clock
+ *
+ * @return 0 on success, negative value on error
+ *
+ */
+int sps_ctrl_bam_dma_clk(bool clk_on);
 #else
 static inline int sps_register_bam_device(const struct sps_bam_props
 			*bam_props, u32 *dev_handle)
@@ -649,6 +658,17 @@ static inline int sps_setup_bam2bam_fifo(struct sps_mem_buffer *mem_buffer,
 }
 
 static inline int sps_get_unused_desc_num(struct sps_pipe *h, u32 *desc_num)
+{
+	return -EPERM;
+}
+
+static inline int sps_get_bam_debug_info(u32 dev, u32 option, u32 para,
+		u32 tb_sel, u32 desc_sel)
+{
+	return -EPERM;
+}
+
+static inline int sps_ctrl_bam_dma_clk(bool clk_on)
 {
 	return -EPERM;
 }
