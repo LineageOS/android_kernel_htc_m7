6aae29063079553b9f7e9b77f2fcb4bf6c7266e6
jared caliendo <kushdeck@gmail.com>
2013-06-28 19:54:09 -0700

    htc: soc: msm: update from Google Edition Source.
    Change-Id: Ia7da7baa65d697789582da42c7d4145fe7d62dba

--- sound/soc/msm/apq8064_M7.c
+++ sound/soc/msm/apq8064_M7.c
@@ -80,10 +80,19 @@
 
 #define TABLA_MBHC_DEF_BUTTONS 8
 #define TABLA_MBHC_DEF_RLOADS 5
-
 #define HAC_PAMP_GPIO	6
+
+#ifdef CONFIG_MACH_DUMMY
+#define RCV_PAMP_PMGPIO	24
+#else
 #define RCV_PAMP_GPIO    67
+#endif
+
+#ifdef CONFIG_MACH_DUMMY
+#define RCV_SPK_SEL_PMGPIO    5
+#else
 #define RCV_SPK_SEL_PMGPIO    24
+#endif
 
 static int msm_hac_control;
 static int aux_pcm_open = 0;
@@ -245,28 +254,28 @@ static int configure_mi2s_rx_gpio(void)
 {
 	int rtn;
 
-	rtn = gpio_request(GPIO_MI2S_RX_SCLK, "MI2S_RX_SCLK");
+	rtn	= gpio_request(GPIO_MI2S_RX_SCLK, "MI2S_RX_SCLK");
 	if (rtn) {
 		pr_err("%s: Failed to request gpio %d\n", __func__,
 			   GPIO_MI2S_RX_SCLK);
 		goto err;
 	}
 
-	rtn = gpio_request(GPIO_MI2S_RX_WS, "MI2S_RX_WS");
+	rtn	= gpio_request(GPIO_MI2S_RX_WS, "MI2S_RX_WS");
 	if (rtn) {
 		pr_err("%s: Failed to request gpio %d\n", __func__,
 			   GPIO_MI2S_RX_WS);
 		goto err;
 	}
 
-	rtn = gpio_request(GPIO_MI2S_RX_DOUT0, "MI2S_RX_DOUT0");
+	rtn	= gpio_request(GPIO_MI2S_RX_DOUT0, "MI2S_RX_DOUT0");
 	if (rtn) {
 		pr_err("%s: Failed to request gpio %d\n", __func__,
 			   GPIO_MI2S_RX_DOUT0);
 		goto err;
 	}
 
-	rtn = gpio_request(GPIO_MI2S_RX_DOUT3, "MI2S_RX_DOUT3");
+	rtn	= gpio_request(GPIO_MI2S_RX_DOUT3, "MI2S_RX_DOUT3");
 	if (rtn) {
 		pr_err("%s: Failed to request gpio %d\n", __func__,
 			   GPIO_MI2S_RX_DOUT3);
@@ -275,7 +284,6 @@ static int configure_mi2s_rx_gpio(void)
 err:
 	return rtn;
 }
-
 static int msm8960_mi2s_startup(struct snd_pcm_substream *substream)
 {
 	int ret = 0;
@@ -322,11 +330,13 @@ static int msm8960_mi2s_startup(struct snd_pcm_substream *substream)
 	return ret;
 }
 
+
 static struct snd_soc_ops msm8960_mi2s_be_ops = {
 	.startup = msm8960_mi2s_startup,
 	.shutdown = msm8960_mi2s_shutdown,
 	.hw_params = msm8960_mi2s_hw_params,
 };
+
 
 static int msm8960_i2s_hw_params(struct snd_pcm_substream *substream,
 			struct snd_pcm_hw_params *params)
@@ -345,11 +355,11 @@ static int msm8960_i2s_hw_params(struct snd_pcm_substream *substream,
 static int msm8960_i2s_tx_free_gpios(void)
 {
 
-	static uint32_t audio_i2s_table[] = {
-		GPIO_CFG(GPIO_I2S_TX_PCLK, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-		GPIO_CFG(GPIO_I2S_TX_WS, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-		GPIO_CFG(GPIO_I2S_TX_DIN, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
-	};
+        static uint32_t audio_i2s_table[] = {
+            GPIO_CFG(GPIO_I2S_TX_PCLK, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
+            GPIO_CFG(GPIO_I2S_TX_WS, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
+            GPIO_CFG(GPIO_I2S_TX_DIN, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
+        };
 
 	gpio_free(GPIO_I2S_TX_DIN);
 	gpio_free(GPIO_I2S_TX_WS);
@@ -387,28 +397,28 @@ static int configure_i2s_tx_gpio(void)
 		GPIO_CFG(GPIO_I2S_TX_DIN, 1, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_8MA),
 	};
 
-	rtn = gpio_request(GPIO_I2S_TX_PCLK, "I2S_TX_PCLK");
+	rtn	= gpio_request(GPIO_I2S_TX_PCLK, "I2S_TX_PCLK");
 	if (rtn) {
 		pr_err("%s: Failed to request gpio %d\n", __func__,
 			   GPIO_I2S_TX_PCLK);
 		goto err;
 	}
-	rtn = gpio_request(GPIO_I2S_TX_WS, "I2S_TX_WS");
+	rtn	= gpio_request(GPIO_I2S_TX_WS, "I2S_TX_WS");
 	if (rtn) {
 		pr_err("%s: Failed to request gpio %d\n", __func__,
 			   GPIO_I2S_TX_WS);
 		goto err;
 	}
-	rtn = gpio_request(GPIO_I2S_TX_DIN, "I2S_TX_DIN");
+	rtn	= gpio_request(GPIO_I2S_TX_DIN, "I2S_TX_DIN");
 	if (rtn) {
 		pr_err("%s: Failed to request gpio %d\n", __func__,
 			   GPIO_I2S_TX_DIN);
 		goto err;
 	}
 
-	gpio_tlmm_config(audio_i2s_table[0], GPIO_CFG_ENABLE);
-	gpio_tlmm_config(audio_i2s_table[1], GPIO_CFG_ENABLE);
-	gpio_tlmm_config(audio_i2s_table[2], GPIO_CFG_ENABLE);
+        gpio_tlmm_config(audio_i2s_table[0], GPIO_CFG_ENABLE);
+        gpio_tlmm_config(audio_i2s_table[1], GPIO_CFG_ENABLE);
+        gpio_tlmm_config(audio_i2s_table[2], GPIO_CFG_ENABLE);
 
 err:
 	return rtn;
@@ -485,7 +495,11 @@ static void msm_ext_spk_power_amp_on(u32 spk)
 
 
 			pr_info("rcv amp on++");
+#ifdef CONFIG_MACH_DUMMY
+			gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_PAMP_PMGPIO), 1);
+#else
 			gpio_direction_output(RCV_PAMP_GPIO, 1);
+#endif
 			gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_SPK_SEL_PMGPIO), 1);
 			pr_info("rcv amp on--");
 
@@ -509,13 +523,13 @@ static void msm_ext_spk_power_amp_on(u32 spk)
 			(msm_hs_pamp & HS_AMP_NEG)) {
 			
 			pr_info("hs amp on++");
-			if (query_tpa6185()) {
-				gpio_direction_output(PM8921_GPIO_PM_TO_SYS(10), 1);
-				set_handset_amp(1);
-			}
+                        if(query_tpa6185()) {
+                            gpio_direction_output(PM8921_GPIO_PM_TO_SYS(10), 1);
+			    set_handset_amp(1);
+                        }
 
-			if (query_rt5501())
-				set_rt5501_amp(1);
+                        if(query_rt5501())
+                            set_rt5501_amp(1);
 			pr_info("hs amp on--");
 			pr_debug("%s: slepping 4 ms after turning on external "
 				" Bottom Speaker Ampl\n", __func__);
@@ -535,6 +549,18 @@ static void msm_ext_spk_power_amp_on(u32 spk)
 
 		if ((msm_ext_bottom_spk_pamp & BOTTOM_SPK_AMP_POS) &&
 			(msm_ext_bottom_spk_pamp & BOTTOM_SPK_AMP_NEG)) {
+#if 0
+			
+			pr_info("hs amp on++");
+                        if(query_tpa6185()) {
+                            gpio_direction_output(PM8921_GPIO_PM_TO_SYS(10), 1);
+			    set_handset_amp(1);
+                        }
+
+                        if(query_rt5501())
+                            set_rt5501_amp(1);
+			pr_info("hs amp on--");
+#endif
 			pr_debug("%s: slepping 4 ms after turning on external "
 				" Bottom Speaker Ampl\n", __func__);
 			usleep_range(4000, 4000);
@@ -580,7 +606,11 @@ static void msm_ext_spk_power_amp_off(u32 spk)
 
 
 		pr_info("rcv amp off ++");
+#ifdef CONFIG_MACH_DUMMY
+		gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_PAMP_PMGPIO), 0);
+#else
 		gpio_direction_output(RCV_PAMP_GPIO, 0);
+#endif
 		gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_SPK_SEL_PMGPIO), 0);
 		pr_info("rcv amp off --");
 
@@ -596,25 +626,37 @@ static void msm_ext_spk_power_amp_off(u32 spk)
 
 		
 		pr_info("hs amp off ++");
-		if (query_tpa6185()) {
-			set_handset_amp(0);
-			gpio_direction_output(PM8921_GPIO_PM_TO_SYS(10), 0);
-		}
+                if(query_tpa6185()) {
+		    set_handset_amp(0);
+                    gpio_direction_output(PM8921_GPIO_PM_TO_SYS(10), 0);
+                }
 
-		if (query_rt5501())
-			set_rt5501_amp(0);
+                if(query_rt5501())
+                    set_rt5501_amp(0);
 		pr_info("hs amp off --");
 
 		msm_hs_pamp = 0;
 
 		pr_debug("%s: sleeping 4 ms after turning off external Bottom"
-				" Speaker Ampl\n", __func__);
+			" Speaker Ampl\n", __func__);
 
 		usleep_range(4000, 4000);
 	} else if (spk & (BOTTOM_SPK_AMP_POS | BOTTOM_SPK_AMP_NEG)) {
 
 		if (!msm_ext_bottom_spk_pamp)
 			return;
+#if 0
+		
+		pr_info("hs amp off ++");
+                if(query_tpa6185()) {
+		    set_handset_amp(0);
+                    gpio_direction_output(PM8921_GPIO_PM_TO_SYS(10), 0);
+                }
+
+                if(query_rt5501())
+                    set_rt5501_amp(0);
+		pr_info("hs amp off --");
+#endif
 		msm_ext_bottom_spk_pamp = 0;
 
 		pr_debug("%s: sleeping 4 ms after turning off external Bottom"
@@ -655,6 +697,82 @@ static void msm_ext_spk_power_amp_off(u32 spk)
 	}
 }
 
+static int msm_get_hac(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol)
+{
+	pr_info("%s: msm_hac_control = %d", __func__, msm_hac_control);
+	ucontrol->value.integer.value[0] = msm_hac_control;
+	return 0;
+}
+static int msm_set_hac(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol)
+{
+	int ret = 0;
+	if (msm_hac_control == ucontrol->value.integer.value[0])
+		return 0;
+
+	msm_hac_control = ucontrol->value.integer.value[0];
+	pr_info("%s()  %d\n", __func__, msm_hac_control);
+	ret = gpio_request(HAC_PAMP_GPIO, "AUDIO_HAC_AMP");
+	if (ret) {
+		pr_err("%s: Error requesting GPIO %d\n", __func__,
+			HAC_PAMP_GPIO);
+			return ret;
+		}
+		else {
+			if (msm_hac_control) {
+				pr_info("%s: enable hac amp gpio %d\n", __func__, HAC_PAMP_GPIO);
+				gpio_direction_output(HAC_PAMP_GPIO, 1);
+			} else {
+				pr_info("%s: disable hac amp gpio %d\n", __func__, HAC_PAMP_GPIO);
+				gpio_direction_output(HAC_PAMP_GPIO, 0);
+			}
+			gpio_free(HAC_PAMP_GPIO);
+		}
+	return 1;
+}
+#if 0
+static int msm_get_rcv_amp(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol)
+{
+	pr_info("%s: msm_rcv_control = %d", __func__, msm_rcv_control);
+	ucontrol->value.integer.value[0] = msm_rcv_control;
+	return 0;
+}
+static int msm_set_rcv_amp(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol)
+{
+	int ret = 0;
+
+	if (msm_rcv_control == ucontrol->value.integer.value[0])
+		return 0;
+
+	msm_rcv_control = ucontrol->value.integer.value[0];
+	pr_info("%s()  %d\n", __func__, msm_rcv_control);
+	ret = gpio_request(RCV_PAMP_GPIO, "AUDIO_RCV_AMP");
+	if (ret) {
+		pr_err("%s: Error requesting GPIO %d\n", __func__,
+			RCV_PAMP_GPIO);
+			return ret;
+		}
+		else {
+			if (msm_rcv_control) {
+				pr_info("%s: enable rcv amp gpio %d\n", __func__, HAC_PAMP_GPIO);
+				usleep_range(20000,20000);
+				ret =gpio_direction_output(RCV_PAMP_GPIO, 1);
+				ret = gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_SPK_SEL_PMGPIO), 1);
+			} else {
+				pr_info("%s: disable rcv amp gpio %d\n", __func__, HAC_PAMP_GPIO);
+				gpio_direction_output(RCV_PAMP_GPIO, 0);
+				gpio_direction_output(PM8921_GPIO_PM_TO_SYS(RCV_SPK_SEL_PMGPIO), 0);
+				usleep_range(20000,20000);
+			}
+			gpio_free(RCV_PAMP_GPIO);
+		}
+	return 1;
+}
+#endif
+
 static void msm_ext_control(struct snd_soc_codec *codec)
 {
 	struct snd_soc_dapm_context *dapm = &codec->dapm;
@@ -690,7 +808,6 @@ static int msm_get_spk(struct snd_kcontrol *kcontrol,
 	ucontrol->value.integer.value[0] = msm_spk_control;
 	return 0;
 }
-
 static int msm_set_spk(struct snd_kcontrol *kcontrol,
 	struct snd_ctl_elem_value *ucontrol)
 {
@@ -704,7 +821,6 @@ static int msm_set_spk(struct snd_kcontrol *kcontrol,
 	msm_ext_control(codec);
 	return 1;
 }
-
 static int msm_spkramp_event(struct snd_soc_dapm_widget *w,
 	struct snd_kcontrol *k, int event)
 {
@@ -935,20 +1051,20 @@ static const struct snd_soc_dapm_route apq8064_common_audio_map[] = {
 	
 	{"AMIC1", NULL, "MIC BIAS1 External"},
 	{"MIC BIAS1 External", NULL, "Analog mic7"},
-
-
-	{"AMIC2", NULL, "MIC BIAS2 External"},
-	{"MIC BIAS2 External", NULL, "Headset Mic"},
-
-
+ 
+	
+ 	{"AMIC2", NULL, "MIC BIAS2 External"},
+ 	{"MIC BIAS2 External", NULL, "Headset Mic"},
+ 
+	
 	{"AMIC3", NULL, "MIC BIAS3 External"},
 	{"MIC BIAS3 External", NULL, "ANCRight Headset Mic"},
-
-	{"AMIC4", NULL, "MIC BIAS1 Internal2"},
-	{"MIC BIAS1 Internal2", NULL, "ANCLeft Headset Mic"},
-
-
-
+ 
+ 	{"AMIC4", NULL, "MIC BIAS1 Internal2"},
+ 	{"MIC BIAS1 Internal2", NULL, "ANCLeft Headset Mic"},
+ 
+        
+	
 };
 
 static const struct snd_soc_dapm_route apq8064_mtp_audio_map[] = {
@@ -1130,6 +1246,12 @@ static const struct snd_kcontrol_new tabla_msm_controls[] = {
 			msm_incall_rec_mode_get, msm_incall_rec_mode_put),
 	SOC_ENUM_EXT("SLIM_3_RX Channels", msm_enum[1],
 		msm_slim_3_rx_ch_get, msm_slim_3_rx_ch_put),
+	SOC_ENUM_EXT("HAC AMP EN", msm_enum[0], msm_get_hac,
+		msm_set_hac),
+#if 0
+	SOC_ENUM_EXT("RCV AMP EN", msm_enum[0], msm_get_rcv_amp,
+		msm_set_rcv_amp),
+#endif
 
 
 };
@@ -1790,7 +1912,7 @@ static int msm_auxpcm_startup(struct snd_pcm_substream *substream)
 
 	aux_pcm_open++;
 
-	if (aux_pcm_open > 1) {
+	if(aux_pcm_open > 1) {
 		mutex_unlock(&aux_pcm_mutex);
 		return 0;
 	}
@@ -1826,7 +1948,7 @@ static void msm_auxpcm_shutdown(struct snd_pcm_substream *substream)
 	mutex_lock(&aux_pcm_mutex);
 	aux_pcm_open--;
 
-	if (aux_pcm_open < 1) {
+	if(aux_pcm_open < 1) {
 		msm_aux_pcm_free_gpios();
 	}
 
@@ -1970,6 +2092,11 @@ static struct snd_soc_dai_link msm_dai[] = {
 		
 	},
 	{
+#if 0
+		.name = "INT_FM Hostless",
+		.stream_name = "INT_FM Hostless",
+		.cpu_dai_name	= "INT_FM_HOSTLESS",
+#endif
 		.name = "PRI_I2S_TX Hostless",
 		.stream_name = "PRI_I2S Hostless",
 		.cpu_dai_name	= "PRI_I2S_HOSTLESS",
@@ -2015,7 +2142,7 @@ static struct snd_soc_dai_link msm_dai[] = {
 		.codec_dai_name = "snd-soc-dummy-dai",
 		.codec_name = "snd-soc-dummy",
 	},
-	/* Backend DAI Links */
+	
 	{
 		.name = LPASS_BE_SLIMBUS_0_RX,
 		.stream_name = "Slimbus Playback",
@@ -2319,6 +2446,7 @@ static struct snd_soc_dai_link msm_dai[] = {
 		.codec_name = "snd-soc-dummy",
 		
 	},
+	
 	{
 		.name = "MSM8960 Media5",
 		.stream_name = "MultiMedia5",
@@ -2389,6 +2517,19 @@ static struct snd_soc_dai_link msm_dai[] = {
 		.be_id = MSM_FRONTEND_DAI_MULTIMEDIA8,
 	},
 	{
+		.name = "Compress Stub",
+		.stream_name = "Compress Stub",
+		.cpu_dai_name	= "MM_STUB",
+		.platform_name  = "msm-pcm-hostless",
+		.dynamic = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		.ignore_pmdown_time = 1, 
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+	},
+	{
 		.name = "VoLTE",
 		.stream_name = "VoLTE",
 		.cpu_dai_name   = "VoLTE",
@@ -2419,20 +2560,6 @@ static struct snd_soc_dai_link msm_dai[] = {
 		.ignore_pmdown_time = 1,
 		.be_id = MSM_FRONTEND_DAI_MULTIMEDIA5,
 	},
-	{
-		.name = "Compress Stub",
-		.stream_name = "Compress Stub",
-		.cpu_dai_name	= "MM_STUB",
-		.platform_name  = "msm-pcm-hostless",
-		.dynamic = 1,
-		.trigger = {SND_SOC_DPCM_TRIGGER_POST, SND_SOC_DPCM_TRIGGER_POST},
-		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
-		.ignore_suspend = 1,
-		.ignore_pmdown_time = 1, 
-		.codec_dai_name = "snd-soc-dummy-dai",
-		.codec_name = "snd-soc-dummy",
-	},
-	
 
 };
 
