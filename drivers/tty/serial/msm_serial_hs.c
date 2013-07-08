99796833d6b4c346c54cadf7ce88400f0f14a069
Ethan Chen <intervigil@gmail.com>
2013-07-07 18:35:05 -0700

    msm_serial_hs: let bluesleep control uart clock
    Change-Id: Ic2664734504085378099c14fe428cf8db2b23725

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -2660,6 +2660,8 @@ static void __exit msm_serial_hs_exit(void)
 	uart_unregister_driver(&msm_hs_driver);
 }
 
+#ifndef CONFIG_BT_MSM_SLEEP
+/* Bluesleep manages uart clk control */
 static int msm_hs_runtime_idle(struct device *dev)
 {
 	/*
@@ -2692,13 +2694,17 @@ static const struct dev_pm_ops msm_hs_dev_pm_ops = {
 	.runtime_resume  = msm_hs_runtime_resume,
 	.runtime_idle    = msm_hs_runtime_idle,
 };
+#endif /* CONFIG_BT_MSM_SLEEP */
 
 static struct platform_driver msm_serial_hs_platform_driver = {
 	.probe	= msm_hs_probe,
 	.remove = __devexit_p(msm_hs_remove),
 	.driver = {
 		.name = "msm_serial_hs",
+#ifndef CONFIG_BT_MSM_SLEEP
+		/* Bluesleep manages uart clk control */
 		.pm   = &msm_hs_dev_pm_ops,
+#endif
 	},
 };
 
