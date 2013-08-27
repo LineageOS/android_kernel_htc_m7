6dcf18a189ac438f51ee9e0edd8b780127bbeff0
Dan Pasanen <dan.pasanen@gmail.com>
2013-08-27 18:33:18 -0500

    clean up logspam ffs
    Change-Id: Iec4bfe0eb60678e2317e556cac98a77737eec0cc

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -66,7 +66,7 @@ static void check_temp(struct work_struct *work)
 				tsens_dev.sensor_num);
 		goto reschedule;
 	} else
-		pr_info("msm_thermal: TSENS sensor %d (%ld C)\n",
+		pr_debug("msm_thermal: TSENS sensor %d (%ld C)\n",
 				tsens_dev.sensor_num, temp);
 	if (temp >= msm_thermal_info.limit_temp) {
 		max_freq = msm_thermal_info.limit_freq;
